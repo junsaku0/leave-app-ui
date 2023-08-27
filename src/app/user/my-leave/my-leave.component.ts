4f886028e76b5dc197a5f891ca766ffc91176e51
presmae <precious.marson@synacy.com>
2023-08-27 12:31:38 +0800

    add logic and error handling my-leave.component.ts

--- src/app/user/my-leave/my-leave.component.ts
+++ src/app/user/my-leave/my-leave.component.ts
@@ -4,17 +4,15 @@ import {LeaveService} from "../service/leave.service";
 import {HttpClient} from "@angular/common/http";
 
 
-
-
-
 @Component({
   selector: 'app-my-leave',
   templateUrl: './my-leave.component.html',
   styleUrls: ['./my-leave.component.css']
 })
 export class MyLeaveComponent implements OnInit {
-    public myLeaves:any
+    public myLeaves:any;
     @Input() userId!:number;
+    @Input() userDetails: any;
 
 constructor(private leaveService: LeaveService, private Http: HttpClient){
 
@@ -25,12 +23,16 @@ fetchMyLeave (userId :number) {
     );
 }
     ngOnInit(): void {
-    let response = this.Http.get("http://localhost:8080/api/v1/leave/head/{id}");
-    response.subscribe((data) => this.myLeaves = data);
-    // this.fetchMyLeave(this.userId)
+    try {
+        let response = this.Http.get("http://localhost:8080/api/v1/leave/head/{id}");
+        response.subscribe((data) => this.myLeaves = data);
+    }catch (error) {
+        console.error('There was an error fetching: ', error);
+
+    }// this.fetchMyLeave(this.userId)
     }
 
-    @Input() userDetails: any;
+
 
 
 }
