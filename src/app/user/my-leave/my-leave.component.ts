7828fb09b8e1955cc2065f77590413be31161932
Ernestkun <ernest.gloria@synacy.com>
2023-08-30 14:04:17 +0800

    added calendar to the apply leave

--- src/app/user/my-leave/my-leave.component.ts
+++ src/app/user/my-leave/my-leave.component.ts
@@ -13,23 +13,30 @@ export class MyLeaveComponent implements OnInit {
     public myLeaves:any;
     @Input() userId!:number;
     @Input() userDetails: any;
+    @Input()managerId!:number;
 
 constructor(private leaveService: LeaveService, private Http: HttpClient){
 
 }
-fetchMyLeave (userId :number) {
-    this.leaveService.fetchPersonalLeave(userId).subscribe(
-        (data) => { this.myLeaves = data},
-    );
-}
+    fetchMyLeave (userId :number) {
+        this.leaveService.fetchPersonalLeave(userId).subscribe(
+            (data) => { this.myLeaves = data},
+        );
+    }
     ngOnInit(): void {
-    try {
-        let response = this.Http.get("http://localhost:8080/api/v1/leave/head/{id}");
-        response.subscribe((data) => this.myLeaves = data);
-    }catch (error) {
-        console.error('There was an error fetching: ', error);
 
-    }// this.fetchMyLeave(this.userId)
+        this.fetchMyLeave(this.userId)
+    }
+
+    public updateEmployeeLeave(leaveId: number, status: any): void {
+        // const updateDetails: LeaveUpdateDetails = {status};
+
+        this.leaveService.updateleave(leaveId, status).subscribe(
+            (response: any) => {
+                // Handle success response here
+                console.log('Leave updated successfully:', response);
+            }
+        );
     }
 
 
